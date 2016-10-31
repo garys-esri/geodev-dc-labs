13c00aa7cfcd8f850adf230c75cb23c2eddd99f4
Gary Sheppard <gsheppard@esri.com>
2016-10-31 14:33:11 -0400

    Initial app (no map)

---
+++ 2016-11-runtime/solutions/iOS/Swift/Ex1_MapAndScene/Ex1_MapAndScene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ex1_MapAndScene
+//
+//  Created by Gary Sheppard on 10/31/16.
+//  Copyright © 2016 Esri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
