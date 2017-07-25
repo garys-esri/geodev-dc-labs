c7fb8d8007bec9dfeb274089d108bfdbfe815dca
Gary Sheppard <gsheppard@esri.com>
2017-07-25 15:35:06 -0400

    Added 3D to Android Exercise 5

--- runtime-workshop/exercises/Android/Exercise 5 Routing.md
+++ runtime-workshop/exercises/Android/Exercise 5 Routing.md
@@ -5,7 +5,7 @@ ArcGIS Runtime features the ability to run ArcGIS geoprocessing for analysis and
 This exercise walks you through the following:
 - Get the user to tap an origin point and a destination point
 - Calculate a driving route from the origin to the destination
-- Display the route on the map
+- Display the route on the map or scene
 
 Prerequisites:
 - Complete [Exercise 4](Exercise%204%20Buffer%20and%20Query.md), or get the Exercise 4 code solution compiling and running properly, preferably in an IDE.
@@ -27,17 +27,20 @@ After doing Exercise 4, this should seem familiar to you.
             new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, 0xC0550055, 5);
     ```
 
-1. Instantiate a `GraphicsOverlay` field. Also declare a non-`final` Point to store the origin point and set it to `null` for now:
+1. Instantiate two `GraphicsOverlay` objects for routing--one for the map and one for the scene--just as you did in a previous exercise. Also declare a non-`final` Point to store the origin point and set it to `null` for now:
 
     ```
-    private final GraphicsOverlay routeGraphics = new GraphicsOverlay();
+    private final GraphicsOverlay mapRouteGraphics = new GraphicsOverlay();
+    private final GraphicsOverlay sceneRouteGraphics = new GraphicsOverlay();
     private Point originPoint = null;
     ```
     
-1. In `onCreate(Bundle)`, add the `GraphicsOverlay` to the `MapView`:
+1. In `onCreate(Bundle)`, add the `GraphicsOverlay`s to the `MapView` and `SceneView` respectively:
 
     ```
-    mapView.getGraphicsOverlays().add(routeGraphics);
+    mapView.getGraphicsOverlays().add(mapRouteGraphics);
+    sceneRouteGraphics.getSceneProperties().setSurfacePlacement(SurfacePlacement.DRAPED);
+    sceneView.getGraphicsOverlays().add(sceneRouteGraphics);
     ```
     
 1. Inside `res/drawable`, create a new drawable resource file called `routing_button.xml`. Android Studio creates this file with a `<selector>` element. Add two `<item>` elements to the selector: one for drawing the button when it is selected, and one for drawing the button when it is not selected. `drawable/routing` and `drawable/routing_selected` refer to images that you added to `res/drawable` in a previous exercise. Your `buffer_and_query_button.xml` should look something like this:
@@ -77,13 +80,13 @@ After doing Exercise 4, this should seem familiar to you.
     private ImageButton imageButton_routing = null;
     ```
     
-1. In `onCreate(Bundle)`, after `setContentView`, set `imageButton_routing` to the `ImageButton` you created in XML, using `findViewById` and a cast:
+1. In `onCreate(Bundle)`, after `setContentView`, set `imageButton_routing` to the `ImageButton` you created in XML, using `findViewById`:
 
     ```
-    imageButton_routing = (ImageButton) findViewById(R.id.imageButton_routing);
+    imageButton_routing = findViewById(R.id.imageButton_routing);
     ``` 
     
-1. In `imageButton_bufferAndQuery_onAction()`, if the buffer and query toggle button is selected, un-select the routing toggle button. _Note: if we had more than two toggle buttons, we would want to do something smarter than this. Since this exercise is not about building a UI, we're naïvely assuming there will only ever be two toggle buttons._
+1. In `imageButton_bufferAndQuery_onClick()`, if the buffer and query toggle button is selected, un-select the routing toggle button. _Note: if we had more than two toggle buttons, we would want to do something smarter than this. Since this exercise is not about building a UI, we're naïvely assuming there will only ever be two toggle buttons._
 
     ```
     imageButton_routing.setSelected(false);
@@ -97,10 +100,10 @@ After doing Exercise 4, this should seem familiar to you.
     }
     ```
     
-1. In `addStopToRoute(MouseEvent)`, get the list of route graphics. Call `getGeoPoint(MotionEvent)` to convert the `MotionEvent` to a geographic `Point`. Remove the point's z-value if present, since a z-value will cause the routing not to work:
+1. In `addStopToRoute(MouseEvent)`, get the list of route graphics, depending on whether or not we are in 3D mode. Call `getGeoPoint(MotionEvent)` to convert the `MotionEvent` to a geographic `Point`. Remove the point's z-value if present, since a z-value will cause the routing not to work:
 
     ```
-    final ListenableList<Graphic> graphics = routeGraphics.getGraphics();
+    final ListenableList<Graphic> graphics = (threeD ? sceneRouteGraphics : mapRouteGraphics).getGraphics();
     Point point = getGeoPoint(singleTapEvent);
     if (point.hasZ()) {
         point = new Point(point.getX(), point.getY(), point.getSpatialReference());
@@ -132,27 +135,32 @@ After doing Exercise 4, this should seem familiar to you.
     originPoint = null;
     ```
     
-1. Fill in the body of `imageButton_routing_onClick(View)`. If the routing toggle button has been un-selected, set the `MapView` `onTouchListener` to a `DefaultMapViewOnTouchListener` with no overridden methods. If the routing toggle button has been selected, un-select `imageButton_bufferAndQuery` and set the `onTouchListener` to a `DefaultMapViewOnTouchListener` with an overridden `onSingleTapConfirmed(MotionEvent)` method. In `onSingleTapConfirmed`, call `addStopToRoute` with the motion event, and return `true` to indicate that the event has been consumed:
+1. Fill in the body of `imageButton_routing_onClick(View)`. If the routing toggle button has been un-selected, set the `MapView` and `SceneView`'s `onTouchListener` to a `DefaultMapViewOnTouchListener` with no overridden methods. If the routing toggle button has been selected, un-select `imageButton_bufferAndQuery` and set the `onTouchListener` to a `DefaultMapViewOnTouchListener` with an overridden `onSingleTapConfirmed(MotionEvent)` method. In `onSingleTapConfirmed`, call `addStopToRoute` with the motion event, and return `true` to indicate that the event has been consumed:
 
     ```
     imageButton_routing.setSelected(!imageButton_routing.isSelected());
+    DefaultMapViewOnTouchListener listener = null;
     if (imageButton_routing.isSelected()) {
-        mapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mapView) {
+        listener = new DefaultMapViewOnTouchListener(this, mapView) {
             @Override
             public boolean onSingleTapConfirmed(MotionEvent event) {
                 addStopToRoute(event);
                 return true;
             }
-        });
+        };
         imageButton_bufferAndQuery.setSelected(false);
     } else {
-        mapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mapView));
+        listener = new DefaultMapViewOnTouchListener(this, mapView);
     }
+    mapView.setOnTouchListener(listener);
+    sceneView.setOnTouchListener(listener);
     ```
 
 1. Run your app. Verify that you can toggle on the routing button, click an origin point, click a destination point, and see both points displayed:
 
-    ![Origin and destination](12-origin-and-destination.png)
+    ![Origin and destination (map)](12-origin-and-destination-map.png)
+
+    ![Origin and destination (scene)](13-origin-and-destination-scene.jpg)
     
 ## Display the route on the map
 
@@ -198,10 +206,8 @@ After doing Exercise 4, this should seem familiar to you.
 1. Write the rest of the code for the `addStopToRoute` method. In that method, you have an `else` block containing two lines of code: a line that adds a graphic, and a line that sets `originPoint` to `null`. Between those two lines, clear the route parameters' stops and add both `originPoint` and `point` to the route parameters` stops:
 
     ```
-    routeParameters.getStops().clear();
-    for (Point p : new Point[]{ originPoint, point }) {
-        routeParameters.getStops().add(new Stop(p));
-    }
+    routeParameters.clearStops();
+    routeParameters.setStops(Arrays.asList(new Stop(originPoint), new Stop(point)));
     ```
     
 1. After adding the stops, call `RouteTask.solveRouteAsync(RouteParameters)` to solve the route asynchronously. That call returns a `ListenableFuture`, on which you can add a done listener. In that listener, call `get()` on the `ListenableFuture`, and then call `getRoutes()`, get the first route, and add it as a graphic. The call to `get()` can throw two different types of exceptions, which you must catch (or catch all `Throwable` objects if you prefer):
@@ -223,11 +229,13 @@ After doing Exercise 4, this should seem familiar to you.
         }
     });
     ```
-    
-1. Compile and run your app. Verify that you can calculate and display a route:
 
-    ![Route](14-route.png)
-    
+1. Compile and run your app. Verify that you can calculate and display a route in both 2D and 3D:
+
+    ![Route in 2D](14-route-map.png)
+
+    ![Route in 3D](15-route-scene.jpg)
+
 ## How did it go?
 
 If you have trouble, **refer to the solution code**, which is linked near the beginning of this exercise. You can also **submit an issue** in this repo to ask a question or report a problem. If you are participating live with Esri presenters, feel free to **ask a question** of the presenters.
--- runtime-workshop/solutions/Android/Ex5_Routing/app/src/main/java/com/esri/wdc/geodev/MainActivity.java
+++ runtime-workshop/solutions/Android/Ex5_Routing/app/src/main/java/com/esri/wdc/geodev/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.data.QueryParameters;
+import com.esri.arcgisruntime.geometry.Geometry;
 import com.esri.arcgisruntime.geometry.GeometryEngine;
 import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.Polygon;
@@ -17,13 +20,19 @@ import com.esri.arcgisruntime.geometry.SpatialReference;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.layers.Layer;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.ArcGISScene;
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.LayerList;
 import com.esri.arcgisruntime.mapping.MobileMapPackage;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.Camera;
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.LayerSceneProperties;
 import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.SceneView;
 import com.esri.arcgisruntime.security.UserCredential;
 import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
 import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
@@ -34,10 +43,18 @@ import com.esri.arcgisruntime.tasks.networkanalysis.RouteTask;
 import com.esri.arcgisruntime.tasks.networkanalysis.Stop;
 import com.esri.arcgisruntime.util.ListenableList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainActivity extends Activity {
+
+    // Exercise 1: Specify elevation service URL
+    private static final String ELEVATION_IMAGE_SERVICE =
+            "http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer";
 
     // Exercise 3: Instantiate mobile map package (MMPK) path
     private static final String MMPK_PATH = Environment.getExternalStorageDirectory().getPath() + "/data/DC_Crime_Data.mmpk";
@@ -63,14 +80,20 @@ public class MainActivity extends Activity {
     // Exercise 1: Declare and instantiate fields
     private MapView mapView = null;
     private ArcGISMap map = new ArcGISMap();
+    private SceneView sceneView = null;
+    private ArcGISScene scene = new ArcGISScene();
+    private ImageButton imageButton_toggle2d3d = null;
+    private boolean threeD = false;
 
     // Exercise 4: Declare fields
     private ImageButton imageButton_bufferAndQuery = null;
-    private final GraphicsOverlay bufferAndQueryGraphics = new GraphicsOverlay();
+    private final GraphicsOverlay bufferAndQueryMapGraphics = new GraphicsOverlay();
+    private final GraphicsOverlay bufferAndQuerySceneGraphics = new GraphicsOverlay();
 
     // Exercise 5: Declare and instantiate fields
     private ImageButton imageButton_routing = null;
-    private final GraphicsOverlay routeGraphics = new GraphicsOverlay();
+    private final GraphicsOverlay mapRouteGraphics = new GraphicsOverlay();
+    private final GraphicsOverlay sceneRouteGraphics = new GraphicsOverlay();
     private Point originPoint = null;
     private RouteTask routeTask = null;
     private RouteParameters routeParameters = null;
@@ -80,7 +103,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Exercise 1: Set up the map
+        // Exercise 1: Set up the 2D map.
         mapView = (MapView) findViewById(R.id.mapView);
         map.setBasemap(Basemap.createNationalGeographic());
         mapView.setMap(map);
@@ -94,23 +117,75 @@ public class MainActivity extends Activity {
                 if (0 < maps.size()) {
                     map = maps.get(0);
                     mapView.setMap(map);
+                    map.addDoneLoadingListener(new Runnable() {
+                        @Override
+                        public void run() {
+                            Viewpoint viewpoint = map.getInitialViewpoint();
+                            if (null != viewpoint) {
+                                mapView.setViewpointAsync(viewpoint);
+                            }
+                        }
+                    });
                 }
                 map.setBasemap(Basemap.createNationalGeographic());
             }
         });
         mmpk.loadAsync();
 
-        // Exercise 4: Set field values
-        imageButton_bufferAndQuery = (ImageButton) findViewById(R.id.imageButton_bufferAndQuery);
+        // Exercise 1: Set up the 3D scene.
+        sceneView = findViewById(R.id.sceneView);
+        scene.setBasemap(Basemap.createImagery());
+        scene.getBaseSurface().getElevationSources().add(new ArcGISTiledElevationSource(ELEVATION_IMAGE_SERVICE));
+        sceneView.setScene(scene);
 
-        // Exercise 4: Add graphics overlay to map
-        mapView.getGraphicsOverlays().add(bufferAndQueryGraphics);
+        // Exercise 3: Open mobile map package and add its layers to the 3D scene.
+        scene.addDoneLoadingListener(new Runnable() {
+            @Override
+            public void run() {
+                final MobileMapPackage mmpk = new MobileMapPackage(MMPK_PATH);
+                mmpk.addDoneLoadingListener(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<ArcGISMap> maps = mmpk.getMaps();
+                        if (0 < maps.size()) {
+                            final ArcGISMap thisMap = maps.get(0);
+                            thisMap.addDoneLoadingListener(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ArrayList<Layer> layers = new ArrayList<>();
+                                    layers.addAll(thisMap.getOperationalLayers());
+                                    thisMap.getOperationalLayers().clear();
+                                    scene.getOperationalLayers().addAll(layers);
+                                    sceneView.setViewpoint(thisMap.getInitialViewpoint());
+                                    Viewpoint viewpoint = sceneView.getCurrentViewpoint(Viewpoint.Type.CENTER_AND_SCALE);
+                                    Point targetPoint = (Point) viewpoint.getTargetGeometry();
+                                    Camera camera = sceneView.getCurrentViewpointCamera()
+                                            .rotateAround(targetPoint, 45.0, 65.0, 0.0);
+                                    sceneView.setViewpointCameraAsync(camera);
+                                }
+                            });
+                            thisMap.loadAsync();
+                        }
+                    }
+                });
+                mmpk.loadAsync();
+            }
+        });
+
+        // Exercise 4: Set field values
+        imageButton_bufferAndQuery = findViewById(R.id.imageButton_bufferAndQuery);
+
+        // Exercise 4: Add graphics overlays to map and scene
+        mapView.getGraphicsOverlays().add(bufferAndQueryMapGraphics);
+        sceneView.getGraphicsOverlays().add(bufferAndQuerySceneGraphics);
 
         // Exercise 5: Set field values
-        imageButton_routing = (ImageButton) findViewById(R.id.imageButton_routing);
+        imageButton_routing = findViewById(R.id.imageButton_routing);
 
-        // Exercise 5: Add route graphics overlay to map
-        mapView.getGraphicsOverlays().add(routeGraphics);
+        // Exercise 5: Add route graphics overlays to map and scene
+        mapView.getGraphicsOverlays().add(mapRouteGraphics);
+        sceneRouteGraphics.getSceneProperties().setSurfacePlacement(LayerSceneProperties.SurfacePlacement.DRAPED);
+        sceneView.getGraphicsOverlays().add(sceneRouteGraphics);
 
         // Exercise 5: Create routing objects
         routeTask = new RouteTask(this, "http://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World");
@@ -119,7 +194,7 @@ public class MainActivity extends Activity {
          * in the source code for simplicity. For security reasons, you would not
          * do it this way in a real app. Instead, you would do one of the following:
          * - Use an OAuth 2.0 user login
-         * - Use an OAuth 2.0 app login (not directly supported in ArcGIS Runtime Quartz as of Beta 2)
+         * - Use an OAuth 2.0 app login
          * - Challenge the user for credentials
          */
         // Don't share this code without removing plain text username and password!!!
@@ -145,6 +220,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         mapView.resume();
+        sceneView.resume();
         super.onResume();
     }
 
@@ -154,6 +230,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
         mapView.pause();
+        sceneView.pause();
         super.onPause();
     }
 
@@ -163,15 +240,32 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         mapView.dispose();
+        sceneView.dispose();
         super.onDestroy();
     }
 
     /**
-     * Exercise 2: Listener for zoom out button.
-     * @param view The button.
+     * Exercise 1: Toggle between 2D map and 3D scene.
      */
-    public void imageButton_zoomOut_onClick(View view) {
-        zoom(0.5);
+    public void imageButton_toggle2d3d_onClick(View view) {
+        threeD = !threeD;
+        setWeight(mapView, threeD ? 1f : 0f);
+        setWeight(sceneView, threeD ? 0f : 1f);
+        if (null == imageButton_toggle2d3d) {
+            imageButton_toggle2d3d = findViewById(R.id.imageButton_toggle2d3d);
+        }
+        imageButton_toggle2d3d.setImageResource(threeD ? R.drawable.two_d : R.drawable.three_d);
+    }
+
+    /**
+     * Exercise 1: Set the weight of a View, e.g. to show or hide it.
+     */
+    private void setWeight(View view, float weight) {
+        final ViewGroup.LayoutParams params = view.getLayoutParams();
+        if (params instanceof LinearLayout.LayoutParams) {
+            ((LinearLayout.LayoutParams) params).weight = weight;
+        }
+        view.setLayoutParams(params);
     }
 
     /**
@@ -183,11 +277,47 @@ public class MainActivity extends Activity {
     }
 
     /**
+     * Exercise 2: Listener for zoom out button.
+     * @param view The button.
+     */
+    public void imageButton_zoomOut_onClick(View view) {
+        zoom(0.5);
+    }
+
+    /**
+     * Exercise 2: Zoom the 2D map.
+     */
+    private void zoomMap(double factor) {
+        mapView.setViewpointScaleAsync(mapView.getMapScale() / factor);
+    }
+
+    /**
+     * Exercise 2: Zoom the 3D scene.
+     */
+    private void zoomScene(double factor) {
+        Geometry target = sceneView.getCurrentViewpoint(Viewpoint.Type.CENTER_AND_SCALE).getTargetGeometry();
+        if (target instanceof Point) {
+            Camera camera = sceneView.getCurrentViewpointCamera()
+                    .zoomToward((Point) target, factor);
+            sceneView.setViewpointCameraAsync(camera, 0.5f);
+        } else {
+            // This shouldn't happen, but in case it does...
+            Logger.getLogger(MainActivity.class.getName()).log(Level.WARNING,
+                    "SceneView.getCurrentViewpoint returned {0} instead of {1}",
+                    new String[] { target.getClass().getName(), Point.class.getName() });
+        }
+    }
+
+    /**
      * Exercise 2: Zoom by a factor.
      * @param factor The zoom factor (0 to 1 to zoom out, > 1 to zoom in).
      */
     private void zoom(double factor) {
-        mapView.setViewpointScaleAsync(mapView.getMapScale() / factor);
+        if (threeD) {
+            zoomScene(factor);
+        } else {
+            zoomMap(factor);
+        }
     }
 
     /**
@@ -198,15 +328,19 @@ public class MainActivity extends Activity {
         imageButton_bufferAndQuery.setSelected(!imageButton_bufferAndQuery.isSelected());
         if (imageButton_bufferAndQuery.isSelected()) {
             imageButton_routing.setSelected(false);
-            mapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mapView) {
+            final DefaultMapViewOnTouchListener listener = new DefaultMapViewOnTouchListener(this, mapView) {
                 @Override
                 public boolean onSingleTapConfirmed(MotionEvent event) {
                     bufferAndQuery(event);
                     return true;
                 }
-            });
+            };
+            mapView.setOnTouchListener(listener);
+            sceneView.setOnTouchListener(listener);
         } else {
-            mapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mapView));
+            final DefaultMapViewOnTouchListener listener = new DefaultMapViewOnTouchListener(this, mapView);
+            mapView.setOnTouchListener(listener);
+            sceneView.setOnTouchListener(listener);
         }
     }
 
@@ -219,14 +353,15 @@ public class MainActivity extends Activity {
         Point geoPoint = getGeoPoint(singleTapEvent);
         geoPoint = (Point) GeometryEngine.project(geoPoint, SpatialReference.create(3857));
         Polygon buffer = GeometryEngine.buffer(geoPoint, 1000.0);
-        ListenableList<Graphic> graphics = bufferAndQueryGraphics.getGraphics();
+        ListenableList<Graphic> graphics = (threeD ? bufferAndQuerySceneGraphics : bufferAndQueryMapGraphics).getGraphics();
         graphics.clear();
         graphics.add(new Graphic(buffer, BUFFER_SYMBOL));
         graphics.add(new Graphic(geoPoint, CLICK_SYMBOL));
-
         QueryParameters query = new QueryParameters();
         query.setGeometry(buffer);
-        LayerList operationalLayers = mapView.getMap().getOperationalLayers();
+        LayerList operationalLayers = threeD ?
+                sceneView.getScene().getOperationalLayers() :
+                mapView.getMap().getOperationalLayers();
         for (Layer layer : operationalLayers) {
             if (layer instanceof FeatureLayer) {
                 ((FeatureLayer) layer).selectFeaturesAsync(query, FeatureLayer.SelectionMode.NEW);
@@ -241,7 +376,9 @@ public class MainActivity extends Activity {
         android.graphics.Point screenPoint = new android.graphics.Point(
                 Math.round(singleTapEvent.getX()),
                 Math.round(singleTapEvent.getY()));
-        return mapView.screenToLocation(screenPoint);
+        return threeD ?
+                sceneView.screenToBaseSurface(screenPoint) :
+                mapView.screenToLocation(screenPoint);
     }
 
     /**
@@ -250,18 +387,21 @@ public class MainActivity extends Activity {
      */
     public void imageButton_routing_onClick(View view) {
         imageButton_routing.setSelected(!imageButton_routing.isSelected());
+        DefaultMapViewOnTouchListener listener = null;
         if (imageButton_routing.isSelected()) {
-            mapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mapView) {
+            listener = new DefaultMapViewOnTouchListener(this, mapView) {
                 @Override
                 public boolean onSingleTapConfirmed(MotionEvent event) {
                     addStopToRoute(event);
                     return true;
                 }
-            });
+            };
             imageButton_bufferAndQuery.setSelected(false);
         } else {
-            mapView.setOnTouchListener(new DefaultMapViewOnTouchListener(this, mapView));
+            listener = new DefaultMapViewOnTouchListener(this, mapView);
         }
+        mapView.setOnTouchListener(listener);
+        sceneView.setOnTouchListener(listener);
     }
 
     /**
@@ -269,7 +409,7 @@ public class MainActivity extends Activity {
      * @param singleTapEvent The single tap event with the stop's geometry.
      */
     private void addStopToRoute(MotionEvent singleTapEvent) {
-        final ListenableList<Graphic> graphics = routeGraphics.getGraphics();
+        final ListenableList<Graphic> graphics = (threeD ? sceneRouteGraphics : mapRouteGraphics).getGraphics();
         Point point = getGeoPoint(singleTapEvent);
         if (point.hasZ()) {
             point = new Point(point.getX(), point.getY(), point.getSpatialReference());
@@ -280,10 +420,8 @@ public class MainActivity extends Activity {
             graphics.add(new Graphic(originPoint, ROUTE_ORIGIN_SYMBOL));
         } else {
             graphics.add(new Graphic(point, ROUTE_DESTINATION_SYMBOL));
-            routeParameters.getStops().clear();
-            for (Point p : new Point[]{ originPoint, point }) {
-                routeParameters.getStops().add(new Stop(p));
-            }
+            routeParameters.clearStops();
+            routeParameters.setStops(Arrays.asList(new Stop(originPoint), new Stop(point)));
             final ListenableFuture<RouteResult> solveFuture = routeTask.solveRouteAsync(routeParameters);
             solveFuture.addDoneListener(new Runnable() {
                 @Override
