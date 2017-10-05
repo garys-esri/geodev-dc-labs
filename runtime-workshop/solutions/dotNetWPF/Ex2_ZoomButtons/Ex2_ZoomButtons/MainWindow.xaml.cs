0891c78e10de5aa68b6483c3df7ac866607b06ef
Jo Fraley <jfraley@esri.com>
2017-10-05 10:11:34 -0500

    Updated .NET Exercise 2

--- runtime-workshop/solutions/dotNetWPF/Ex2_ZoomButtons/Ex2_ZoomButtons/MainWindow.xaml.cs
+++ runtime-workshop/solutions/dotNetWPF/Ex2_ZoomButtons/Ex2_ZoomButtons/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.Data;
 using Esri.ArcGISRuntime.Portal;
 using Esri.ArcGISRuntime.Security;
+using Esri.ArcGISRuntime.UI;
 
 using System;
 using System.Diagnostics;
@@ -19,7 +20,7 @@ namespace Ex1_MapAndScene
         "http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer";
         private Scene myScene = null;
         private bool threeD = false;
-
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -30,7 +31,8 @@ namespace Ex1_MapAndScene
         private void Initialize()
         {
             //Exercise 1: Create new Map with basemap and initial location
-            myMap = new Map(Basemap.CreateTopographicVector());
+            myMap = new Map(Basemap.CreateStreetsVector());
+
             //Exercise 1: Assign the map to the MapView
             mapView.Map = myMap;
         }
@@ -41,11 +43,11 @@ namespace Ex1_MapAndScene
             ViewButton.Content = FindResource(ViewButton.Content == FindResource("3D") ? "2D" : "3D");
             if (ViewButton.Content == FindResource("2D"))
             {
-                threeD = true;
+                
                 if (myScene == null)
                 {
                     //Create a new scene
-                    myScene = new Scene(Basemap.CreateNationalGeographic());
+                    myScene = new Scene(Basemap.CreateImageryWithLabels());
                     sceneView.Scene = myScene;
                     // create an elevation source
                     var elevationSource = new ArcGISTiledElevationSource(new System.Uri(ELEVATION_IMAGE_SERVICE));
@@ -58,12 +60,46 @@ namespace Ex1_MapAndScene
                 //Once the scene has been created hide the mapView and show the sceneView
                 mapView.Visibility = Visibility.Hidden;
                 sceneView.Visibility = Visibility.Visible;
+                threeD = true;
             }
             else
             {
-                threeD = false;
+                
                 sceneView.Visibility = Visibility.Hidden;
                 mapView.Visibility = Visibility.Visible;
+                threeD = false;
+            }
+        }
+
+        //Exercise 2 Adding Lock Focus Button Click
+        private void LockButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Change button to lock or lock_selected when button is clicked
+            LockButton.Content = FindResource(LockButton.Content == FindResource("LockFocusSelected") ? "LockFocus" : "LockFocusSelected");
+            if (LockButton.Content == FindResource("LockFocusSelected"))
+            {
+                Geometry target = sceneView.GetCurrentViewpoint(ViewpointType.CenterAndScale).TargetGeometry;
+                if (target.GeometryType == GeometryType.Point)
+                {
+                    Esri.ArcGISRuntime.Geometry.MapPoint targetPoint = (Esri.ArcGISRuntime.Geometry.MapPoint)target;
+                    Camera currentCamera = sceneView.Camera;
+                    Esri.ArcGISRuntime.Geometry.MapPoint currentCameraPoint = currentCamera.Location;
+                    if (null != currentCameraPoint)
+                    { 
+                        double xyDistance = GeometryEngine.DistanceGeodetic(targetPoint, currentCameraPoint, LinearUnits.Meters, AngularUnits.Degrees, GeodeticCurveType.Geodesic).Distance;
+                        double zDistance = currentCameraPoint.Z;
+                        double distanceToTarget = Math.Sqrt(Math.Pow(xyDistance, 2.0) + Math.Pow(zDistance, 2.0));
+
+                        OrbitLocationCameraController cameraController = new OrbitLocationCameraController((MapPoint)target, distanceToTarget);
+                        cameraController.CameraHeadingOffset = currentCamera.Heading;
+                        cameraController.CameraPitchOffset = currentCamera.Pitch;
+                        sceneView.CameraController = cameraController;
+                    }
+                }
+            }
+            else
+            {
+                sceneView.CameraController = new GlobeCameraController();
             }
         }
         //Exercise 2
